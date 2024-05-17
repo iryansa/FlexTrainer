5361025bbfdb39d1931844afaa9f160049d12a75
Siddique Ahmad Ryan <ryan.ahmad2923@gmail.com>
2024-05-17 17:57:39 +0500

    SQL File upload
    Uploading the Sql file containing all the tables and triggers and adding all the necessary details to the Readme

---
+++ FlexTrainerTables.sql
@@ -0,0 +1,465 @@
+CREATE DATABASE PROJECT;
+use PROJECT;
+
+create table users(
+	username varchar(20) primary key,
+	Name varchar(50) not null,
+	email varchar(50) not null,
+	password varchar(20) not null,
+	typeuser varchar(10) not null,
+	expertise varchar(50),
+	qualification varchar(50)
+	);
+
+
+Create table workoutPlan(
+	workout_id INT NOT NULL IDENTITY PRIMARY KEY,
+	workout_name varchar(50) not null,
+	goal varchar(50) not null,
+	exp_level varchar(50) not null,
+	author varchar(20) not null,
+	authortype varchar(10) not null,
+	schedule varchar(10) not null
+	
+	);
+
+create table excercise(
+	workoutday int not null,
+	workoutId int not null,
+	exercisename varchar(50) not null,
+	machine varchar(50),
+	sets int,
+	reps int,
+	musclegroup varchar(50),
+
+	foreign key (workoutId) references workoutPlan(workout_id),
+	primary key(workoutday, workoutId, exercisename)
+	);
+
+create table dietPlan(
+	diet_id INT NOT NULL IDENTITY PRIMARY KEY,
+	diet_name varchar(50) not null,
+	goal varchar(50) not null,
+	typeDiet varchar(50) not null,
+	calories int not null,
+	author varchar(20) not null,
+	authortype varchar(10) not null
+	);
+
+create table meal(
+	dietday int not null,
+	diet_Id int not null,
+	mealdescription varchar(50),
+	kcal int,
+	allergens varchar(50),
+	foreign key (diet_Id) references dietPlan(diet_id),
+	primary key(dietday, diet_Id)
+	);
+
+
+CREATE TABLE appointmentTable (
+	apt_id INT IDENTITY PRIMARY KEY,
+	trainer_username varchar(20) NOT NULL,
+	member_username varchar(20),
+	time_slot datetime NOT NULL,
+	Status varchar(20) NOT NULL 
+
+	FOREIGN KEY (trainer_username) REFERENCES users(username),
+	--FOREIGN KEY (member_username) REFERENCES users(username)
+);
+
+-- create first ***********************
+create table trainerFeedback (
+	apt_id INT PRIMARY KEY,
+	rating INT NOT NULL,
+	description varchar(50)
+
+	FOREIGN KEY (apt_id) REFERENCES appointmentTable(apt_id)
+);
+
+
+
+create table memberworkout(
+	memberusername varchar(20),
+	workoutId int
+
+	foreign key (memberusername) references users(username),
+	foreign key (workoutId) references workoutPlan(workout_id),
+	primary key (memberusername , workoutId)
+	);
+
+create table memberdiet(
+	memberusername varchar(20),
+	dietId int
+
+	foreign key (memberusername) references users(username),
+	foreign key (dietId) references dietPlan(diet_id),
+	primary key (memberusername , dietId)
+	);
+
+
+create table gyms(
+	gymowner varchar(20),
+	gymname varchar(50),
+	businessplan varchar(50),
+	facility varchar(50),
+	currentmembers int,
+	approval varchar(20),
+
+	foreign key (gymowner) references users(username),
+	primary key(gymowner, gymname)
+	)
+
+
+
+
+create table membergym(
+	username varchar(20),
+	gymname varchar(50),
+
+	foreign key(username) references users(username),
+	primary key(username, gymname)
+	)
+	
+create table trainergym(
+	username varchar(20),
+	gymname varchar(50),
+	approved varchar(50),
+
+	foreign key(username) references users(username),
+	primary key(username, gymname)
+	)
+
+
+
+		-- Create Audit Trail Table
+CREATE TABLE AuditTrail (
+    AuditTrailID INT IDENTITY PRIMARY KEY,
+    TableName VARCHAR(50) NOT NULL,
+    Operation VARCHAR(10) NOT NULL,
+    AuditDateTime DATETIME DEFAULT CURRENT_TIMESTAMP
+);
+
+drop table AuditTrail
+
+CREATE TRIGGER Users_AuditTrigger
+ON users
+AFTER INSERT, UPDATE, DELETE
+AS
+BEGIN
+    DECLARE @Operation VARCHAR(10);
+    DECLARE @TableName VARCHAR(50) = 'users';
+    
+    IF EXISTS (SELECT * FROM inserted)
+    BEGIN
+        IF EXISTS (SELECT * FROM deleted)
+            SET @Operation = 'UPDATE';
+        ELSE
+            SET @Operation = 'INSERT';
+    END
+    ELSE
+        SET @Operation = 'DELETE';
+    
+    INSERT INTO AuditTrail (TableName, Operation)
+    VALUES (@TableName, @Operation);
+END;
+
+
+CREATE TRIGGER WorkoutPlan_InsertAuditTrigger
+ON workoutPlan
+AFTER INSERT
+AS
+BEGIN
+    DECLARE @Operation VARCHAR(10) = 'INSERT';
+    DECLARE @TableName VARCHAR(50) = 'workoutPlan';
+    
+    INSERT INTO AuditTrail (TableName, Operation)
+    VALUES (@TableName, @Operation);
+END;
+
+CREATE TRIGGER DietPlan_InsertAuditTrigger
+ON dietPlan
+AFTER INSERT
+AS
+BEGIN
+    DECLARE @Operation VARCHAR(10) = 'INSERT';
+    DECLARE @TableName VARCHAR(50) = 'dietPlan';
+    
+    INSERT INTO AuditTrail (TableName, Operation)
+    VALUES (@TableName, @Operation);
+END;
+
+
+-- Trigger for appointmentTable Insertion
+CREATE TRIGGER Insert_AppointmentTable_AuditTrigger
+ON appointmentTable
+AFTER INSERT
+AS
+BEGIN
+    DECLARE @Operation VARCHAR(10) = 'INSERT';
+    DECLARE @TableName VARCHAR(50) = 'appointmentTable';
+    
+    INSERT INTO AuditTrail (TableName, Operation)
+    VALUES (@TableName, @Operation);
+END;
+
+-- Trigger for appointmentTable Update
+CREATE TRIGGER Update_AppointmentTable_AuditTrigger
+ON appointmentTable
+AFTER UPDATE
+AS
+BEGIN
+    DECLARE @Operation VARCHAR(10) = 'UPDATE';
+    DECLARE @TableName VARCHAR(50) = 'appointmentTable';
+    
+    INSERT INTO AuditTrail (TableName, Operation)
+    VALUES (@TableName, @Operation);
+END;
+
+-- Trigger for appointmentTable Deletion
+CREATE TRIGGER Delete_AppointmentTable_AuditTrigger
+ON appointmentTable
+AFTER DELETE
+AS
+BEGIN
+    DECLARE @Operation VARCHAR(10) = 'DELETE';
+    DECLARE @TableName VARCHAR(50) = 'appointmentTable';
+    
+    INSERT INTO AuditTrail (TableName, Operation)
+    VALUES (@TableName, @Operation);
+END;
+
+-- Trigger for trainerFeedback Insertion
+CREATE TRIGGER Insert_TrainerFeedback_AuditTrigger
+ON trainerFeedback
+AFTER INSERT
+AS
+BEGIN
+    DECLARE @Operation VARCHAR(10) = 'INSERT';
+    DECLARE @TableName VARCHAR(50) = 'trainerFeedback';
+    
+    INSERT INTO AuditTrail (TableName, Operation)
+    VALUES (@TableName, @Operation);
+END;
+
+-- Trigger for trainerFeedback Update
+CREATE TRIGGER Update_TrainerFeedback_AuditTrigger
+ON trainerFeedback
+AFTER UPDATE
+AS
+BEGIN
+    DECLARE @Operation VARCHAR(10) = 'UPDATE';
+    DECLARE @TableName VARCHAR(50) = 'trainerFeedback';
+    
+    INSERT INTO AuditTrail (TableName, Operation)
+    VALUES (@TableName, @Operation);
+END;
+
+-- Trigger for trainerFeedback Deletion
+CREATE TRIGGER Delete_TrainerFeedback_AuditTrigger
+ON trainerFeedback
+AFTER DELETE
+AS
+BEGIN
+    DECLARE @Operation VARCHAR(10) = 'DELETE';
+    DECLARE @TableName VARCHAR(50) = 'trainerFeedback';
+    
+    INSERT INTO AuditTrail (TableName, Operation)
+    VALUES (@TableName, @Operation);
+END;
+
+-- Trigger for memberworkout Insertion
+CREATE TRIGGER Insert_MemberWorkout_AuditTrigger
+ON memberworkout
+AFTER INSERT
+AS
+BEGIN
+    DECLARE @Operation VARCHAR(10) = 'INSERT';
+    DECLARE @TableName VARCHAR(50) = 'memberworkout';
+    
+    INSERT INTO AuditTrail (TableName, Operation)
+    VALUES (@TableName, @Operation);
+END;
+
+-- Trigger for memberworkout Update
+CREATE TRIGGER Update_MemberWorkout_AuditTrigger
+ON memberworkout
+AFTER UPDATE
+AS
+BEGIN
+    DECLARE @Operation VARCHAR(10) = 'UPDATE';
+    DECLARE @TableName VARCHAR(50) = 'memberworkout';
+    
+    INSERT INTO AuditTrail (TableName, Operation)
+    VALUES (@TableName, @Operation);
+END;
+
+-- Trigger for memberworkout Deletion
+CREATE TRIGGER Delete_MemberWorkout_AuditTrigger
+ON memberworkout
+AFTER DELETE
+AS
+BEGIN
+    DECLARE @Operation VARCHAR(10) = 'DELETE';
+    DECLARE @TableName VARCHAR(50) = 'memberworkout';
+    
+    INSERT INTO AuditTrail (TableName, Operation)
+    VALUES (@TableName, @Operation);
+END;
+
+-- Trigger for memberdiet Insertion
+CREATE TRIGGER Insert_MemberDiet_AuditTrigger
+ON memberdiet
+AFTER INSERT
+AS
+BEGIN
+    DECLARE @Operation VARCHAR(10) = 'INSERT';
+    DECLARE @TableName VARCHAR(50) = 'memberdiet';
+    
+    INSERT INTO AuditTrail (TableName, Operation)
+    VALUES (@TableName, @Operation);
+END;
+
+-- Trigger for memberdiet Update
+CREATE TRIGGER Update_MemberDiet_AuditTrigger
+ON memberdiet
+AFTER UPDATE
+AS
+BEGIN
+    DECLARE @Operation VARCHAR(10) = 'UPDATE';
+    DECLARE @TableName VARCHAR(50) = 'memberdiet';
+    
+    INSERT INTO AuditTrail (TableName, Operation)
+    VALUES (@TableName, @Operation);
+END;
+
+-- Trigger for memberdiet Deletion
+CREATE TRIGGER Delete_MemberDiet_AuditTrigger
+ON memberdiet
+AFTER DELETE
+AS
+BEGIN
+    DECLARE @Operation VARCHAR(10) = 'DELETE';
+    DECLARE @TableName VARCHAR(50) = 'memberdiet';
+    
+    INSERT INTO AuditTrail (TableName, Operation)
+    VALUES (@TableName, @Operation);
+END;
+
+
+-- Trigger for gyms Insertion
+CREATE TRIGGER Insert_Gyms_AuditTrigger
+ON gyms
+AFTER INSERT
+AS
+BEGIN
+    DECLARE @Operation VARCHAR(10) = 'INSERT';
+    DECLARE @TableName VARCHAR(50) = 'gyms';
+    
+    INSERT INTO AuditTrail (TableName, Operation)
+    VALUES (@TableName, @Operation);
+END;
+
+-- Trigger for gyms Update
+CREATE TRIGGER Update_Gyms_AuditTrigger
+ON gyms
+AFTER UPDATE
+AS
+BEGIN
+    DECLARE @Operation VARCHAR(10) = 'UPDATE';
+    DECLARE @TableName VARCHAR(50) = 'gyms';
+    
+    INSERT INTO AuditTrail (TableName, Operation)
+    VALUES (@TableName, @Operation);
+END;
+
+-- Trigger for gyms Deletion
+CREATE TRIGGER Delete_Gyms_AuditTrigger
+ON gyms
+AFTER DELETE
+AS
+BEGIN
+    DECLARE @Operation VARCHAR(10) = 'DELETE';
+    DECLARE @TableName VARCHAR(50) = 'gyms';
+    
+    INSERT INTO AuditTrail (TableName, Operation)
+    VALUES (@TableName, @Operation);
+END;
+
+-- Trigger for membergym Insertion
+CREATE TRIGGER Insert_MemberGym_AuditTrigger
+ON membergym
+AFTER INSERT
+AS
+BEGIN
+    DECLARE @Operation VARCHAR(10) = 'INSERT';
+    DECLARE @TableName VARCHAR(50) = 'membergym';
+    
+    INSERT INTO AuditTrail (TableName, Operation)
+    VALUES (@TableName, @Operation);
+END;
+
+-- Trigger for membergym Update
+CREATE TRIGGER Update_MemberGym_AuditTrigger
+ON membergym
+AFTER UPDATE
+AS
+BEGIN
+    DECLARE @Operation VARCHAR(10) = 'UPDATE';
+    DECLARE @TableName VARCHAR(50) = 'membergym';
+    
+    INSERT INTO AuditTrail (TableName, Operation)
+    VALUES (@TableName, @Operation);
+END;
+
+-- Trigger for membergym Deletion
+CREATE TRIGGER Delete_MemberGym_AuditTrigger
+ON membergym
+AFTER DELETE
+AS
+BEGIN
+    DECLARE @Operation VARCHAR(10) = 'DELETE';
+    DECLARE @TableName VARCHAR(50) = 'membergym';
+    
+    INSERT INTO AuditTrail (TableName, Operation)
+    VALUES (@TableName, @Operation);
+END;
+
+-- Trigger for trainergym Insertion
+CREATE TRIGGER Insert_TrainerGym_AuditTrigger
+ON trainergym
+AFTER INSERT
+AS
+BEGIN
+    DECLARE @Operation VARCHAR(10) = 'INSERT';
+    DECLARE @TableName VARCHAR(50) = 'trainergym';
+    
+    INSERT INTO AuditTrail (TableName, Operation)
+    VALUES (@TableName, @Operation);
+END;
+
+-- Trigger for trainergym Update
+CREATE TRIGGER Update_TrainerGym_AuditTrigger
+ON trainergym
+AFTER UPDATE
+AS
+BEGIN
+    DECLARE @Operation VARCHAR(10) = 'UPDATE';
+    DECLARE @TableName VARCHAR(50) = 'trainergym';
+    
+    INSERT INTO AuditTrail (TableName, Operation)
+    VALUES (@TableName, @Operation);
+END;
+
+-- Trigger for trainergym Deletion
+CREATE TRIGGER Delete_TrainerGym_AuditTrigger
+ON trainergym
+AFTER DELETE
+AS
+BEGIN
+    DECLARE @Operation VARCHAR(10) = 'DELETE';
+    DECLARE @TableName VARCHAR(50) = 'trainergym';
+    
+    INSERT INTO AuditTrail (TableName, Operation)
+    VALUES (@TableName, @Operation);
+END;
--- README.md
+++ README.md
@@ -1,6 +1,13 @@
-# FlexTrainer
+# ![FlexTrainer](Flextrainer/barbell.ico) FlexTrainer
 FlexTrainer is a C# Winforms application developed in Visual Studio and use MS SQL as backend.
 The application is an implementation of Gym Management and Fitness tracking system in which different types of users like Gym members, trainers, gym owners, and administrators can join the available gyms, get approval, create or follow existing workout and diet plans, book trainers, etc.
 
+# Before Running
+Remember to create all the necessary tables and triggers using the FlexTrainerTables.sql file.
+Also, You need to first insert data of the admins into the users table manually, otherwise it could cause problems. 
+
 # How to run
-If you want to run the application, then run the FlexTrainer.exe file in the bin/Debug forlder. If you want to run the solution then you should run the FlexTrainer.sln file (Visual Studio solution file).
+If you want to run the application, then run the FlexTrainer.exe file in the bin/Debug forlder. If you want to run the solution then you should run the FlexTrainer.sln file (Visual Studio solution file). After opening the solution in the Microsoft Visual Studio, copy and paste your connection string to all the cs files where it is needed (Comments are added where it is needed).
+
+# Problem you may face
+While copying and pasting the connection string, if the last attribute is Trust Server Certificate = true, then remove the spaces between it to make it look like: TrustServerCertificate=true. Also, While running the application, do not enter ' into the textboxes, as it may cause exceptions while submiting. 
